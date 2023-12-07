711387b9e28bcb2e211ea301d13d96d58ad92872
Eugeniy Maximov <naotta@gmail.com>
2023-12-07 17:43:00 +0600

    fix api querry. start refactoring

--- vacancy_searcher_3000.py
+++ vacancy_searcher_3000.py
@@ -33,11 +33,11 @@ remote_words = str(os.getenv('REMOTE')).split(',')
 #                          'линии поддержки', 'ozon', 'wildberries', 'маркетплейс', 'system', 'писатель',
 #                          'безопасность','куратор','golang','оператор','financial','информационной безопасности']
 
-remote_words = str(os.getenv('EXCLUDE_WORDS')).split(',')
+exclude_words_IN_NAME = str(os.getenv('EXCLUDE_WORDS')).split(',')
 
 # число календарных дней за которое подтягиваем историю загрузки вакансий, брала с запасом, чтобы ничего не упустить, 
 # но большинство вакансий повторяются, так что на увеличении времени это не сильно сказывается (начиная со второго прогона кода)
-days_to_look = 2
+days_to_look = 1
 
 # т.к. я искала 9 вакансий по 9 разным странам, плюс страны для удаленки прогоняются дважды, 
 # то у меня выходило 108 запросов, и при прохождении каждых 10 код выдает print, чтобы понимать сколько осталось
@@ -52,14 +52,14 @@ import pandas as pd
 print('VACANCIES_TO_LOOK:', vacancies_to_look_in_name)
 print('COUNTRIES:', countries_and_schedule)
 print('REMOTE:', remote_words)
-print('EXCLUDE_WORDS:', remote_words)
+print('EXCLUDE_WORDS:', exclude_words_IN_NAME)
 
 quit()
 
 vacancies_to_look_in_name = ['NAME:'+x for x in vacancies_to_look_in_name]
 n_vacancies = len(vacancies_to_look_in_name)
 all_countries = sum(list(list(countries_and_schedule.values())+[countries_and_schedule['remote']]), [])
-n_countries = len(all_countries)
+n_countries = len(countries)
 countries_iteration = sum([[x]*n_vacancies for x in all_countries],[])
 n_remote = len(countries_and_schedule['remote'])
 all_schedules = ['remote']*n_remote*n_vacancies+[None]*\
@@ -67,18 +67,19 @@ all_schedules = ['remote']*n_remote*n_vacancies+[None]*\
 remote_words_there = 0
 
 
-def getPage(page = 0, vacancy_type = 0):
+def getPage(countr = 113, page = 0):
 
     all_params = {
-        'text': vacancies_to_look_in_name*n_countries, # Текст поиска
-        'schedule': all_schedules,
-        'period': [days_to_look]*n_vacancies*n_countries, # 1 - за сутки; 3 - за 3 дня, 7 - за неделю
-        'area': countries_iteration, 
-        'page': [page]*n_vacancies*n_countries, # Индекс страницы поиска на HH
-        'per_page': [100]*n_vacancies*n_countries # Кол-во вакансий на 1 странице
+        'text': ' OR '.join(vacancies_to_look_in_name), # Текст поиска
+        'schedule': 'remote',
+        'period': days_to_look, # 1 - за сутки; 3 - за 3 дня, 7 - за неделю
+        'area': '40', # one area id for each querry
+        'page': page, # Индекс страницы поиска на HH
+        'per_page': 100 # Кол-во вакансий на 1 странице
     }
 
-    params = {x:all_params[x][vacancy_type] for x in all_params.keys()}
+    # params = {x:all_params[x][vacancy_type] for x in all_params.keys()}
+    params = all_params
 
     req = requests.get('https://api.hh.ru/vacancies', params) # Посылаем запрос к API
     data = req.content.decode() # Декодируем его ответ, чтобы Кириллица отображалась корректно
@@ -89,7 +90,8 @@ all_v=[]
 try:
     all_vc = pd.read_csv('all_vacancies.csv')
 except:
-    all_vc = pd.DataFrame(columns=['vacancy_url'])
+    pass
+
 new_df = pd.DataFrame(columns=['name','exp','url','descr','sch','loc'])
 try:
     for dir in ['docs/vacancies', 'docs/pagination']:
@@ -106,25 +108,36 @@ pd.set_option('display.max_colwidth', 10000)
 
 
 # проходимся по всем типам вакансий
-for i in range(n_vacancies*n_countries):
+for country in range(n_countries):
     # проходимся по всем страницам поиска
     for page in range(0, 100):
         # Преобразуем текст ответа запроса в справочник Python
-        jsObj = json.loads(getPage(page, i))
-        
-        # Сохраняем файлы в папку {путь до текущего документа со скриптом}\docs\pagination
-        # Определяем количество файлов в папке для сохранения документа с ответом запроса
-        # Полученное значение используем для формирования имени документа
-        nextFileName = './docs/pagination/{}.json'.format(len(os.listdir('./docs/pagination')))
-        
-        # Создаем новый документ, записываем в него ответ запроса, после закрываем
-        f = open(nextFileName, mode='w', encoding='utf8')
-        f.write(json.dumps(jsObj, ensure_ascii=False))
-        f.close()
+        jsObj = json.loads(getPage(country, page))
         
         # Проверка на последнюю страницу, если вакансий меньше 10000
         if (jsObj['pages'] - page) <= 1:
             break
+
+        for v in jsObj['items']:
+            if (v['url'] not in list(all_vc['vacancy_url'])) and (v['url'] not in all_v): # fix
+            
+                # Обращаемся к API и получаем детальную информацию по конкретной вакансии
+                all_v.append(v['url'])
+                req = requests.get(v['url'])
+                data = req.content.decode()
+                req.close()
+                
+                # Создаем файл в формате json с идентификатором вакансии в качестве названия
+                # Записываем в него ответ запроса и закрываем файл
+                fileName = './docs/vacancies/{}.json'.format(v['id'])
+                f = open(fileName, mode='w', encoding='utf8')
+                f.write(data)
+                f.close()
+                
+                time.sleep(0.25)
+            else:
+                pass
+        
         
         # Необязательная задержка, но чтобы не нагружать сервисы hh, оставим
         time.sleep(0.25)
@@ -202,4 +215,4 @@ for i in range(n_vacancies*n_countries):
             print('осталось '+str(n_vacancies*n_countries - i)+' типов вакансий', end='')
         print('\r', end='')
 
-print('\n\nВсего '+str(len(new_df))+' новых вакансий')
+print('\n\n Всего '+str(len(new_df))+' новых вакансий')
