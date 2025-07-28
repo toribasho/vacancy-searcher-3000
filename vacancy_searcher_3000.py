8b7c0999fa6be6ec521e7115738bb5a1d680001c
Eugeniy Maximov <naotta@gmail.com>
2025-07-28 16:49:25 +0400

    update db setings, add expirience filter

--- vacancy_searcher_3000.py
+++ vacancy_searcher_3000.py
@@ -7,6 +7,11 @@ days_to_look = 2
 user_id = os.getenv('USER_ID')
 user_query = os.getenv('QUERY_ID')
 
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_pw = os.getenv('DB_PASS')
+db_host = os.getenv('DB_HOST')
+
 if ( user_id is None ):  ## external params
     print('No user_id is provided! Aborting...')
     quit()
@@ -37,10 +42,10 @@ import psycopg2
 try:
     # Establish a connection
     conn = psycopg2.connect(
-        dbname="postgres",
-        user="tori",
-        password="112233",
-        host="192.168.1.160",
+        dbname=db_name,
+        user=db_user,
+        password=db_pw,
+        host=db_host,
         port="5432"
     )
     print("Connection successful!")
@@ -69,6 +74,7 @@ with conn.cursor() as cursor:
         countries_and_schedule = {'remote':countries, 'all':schedule}
         remote_words=row[5].strip("'").split(',')
         exclude_words_IN_NAME=row[6].strip("'").split(',')
+        needed_expirience=row[7].strip("'").split(',')
     cursor.close()
 
 print('VACANCIES_TO_LOOK:', vacancies_to_look_in_name)
@@ -77,6 +83,7 @@ print('VACANCIES_TO_LOOK:', vacancies_to_look_in_name)
 print('COUNTRIES & SCHED:', countries_and_schedule)
 print('REMOTE:', remote_words)
 print('EXCLUDE_WORDS:', exclude_words_IN_NAME)
+print('EXPIRIENCE_NEEDED:', needed_expirience)
 print('PROCESSED URLs COUNT:', len(all_v))
 
 vacancies_to_look_in_name = ['NAME:'+x for x in vacancies_to_look_in_name]
@@ -127,47 +134,49 @@ def getVacnciesByType( l_countries, schedule = 'remote' ):
                     # remote_words_there=1 #if any(word in jsonObj['description'].lower() for word in remote_words) else 0
 
                     # if remote_words_there:
-                    if not any(word in jsonObj['name'].lower() for word in exclude_words_IN_NAME):
 
-                        stri = jsonObj['description']
-                        # stri = stri.replace('<strong>','\033[1m')
-                        # stri = stri.replace('</strong>','\033[0m')
-                        stri = stri.replace('</li> <li>','\n')
-                        stri = stri.replace('<ul> <li>','\n')
-                        stri = stri.replace('</li>','\n')
-                        stri = stri.replace('</ul>','')
+                    if jsonObj['experience']['id'] in needed_expirience:
+                        if not any(word in jsonObj['name'].lower() for word in exclude_words_IN_NAME):
+
+                            stri = jsonObj['description']
+                            # stri = stri.replace('<strong>','\033[1m')
+                            # stri = stri.replace('</strong>','\033[0m')
+                            stri = stri.replace('</li> <li>','\n')
+                            stri = stri.replace('<ul> <li>','\n')
+                            stri = stri.replace('</li>','\n')
+                            stri = stri.replace('</ul>','')
 
 
-                        # (user_id,query_id,url,name,experience,alternate_url,description,schedule,location)
-                        # print('Vac name: ', jsonObj['name'])
-                        # print('Exp: ', jsonObj['experience']['id'])
-                        # print('url: ', jsonObj['alternate_url'])
-                        # print('Desc: ', stri)     
-                        # print('Sched: ', jsonObj['schedule']['id'])
-                        # print('Location: ', jsonObj['area']['name'])
+                            # (user_id,query_id,url,name,experience,alternate_url,description,schedule,location)
+                            # print('Vac name: ', jsonObj['name'])
+                            # print('Exp: ', jsonObj['experience']['id'])
+                            # print('url: ', jsonObj['alternate_url'])
+                            # print('Desc: ', stri)     
+                            # print('Sched: ', jsonObj['schedule']['id'])
+                            # print('Location: ', jsonObj['area']['name'])
 
-                        vacancy_data = (
-                            user_id,
-                            user_query,
-                            v['url'],
-                            jsonObj['name'], 
-                            jsonObj['experience']['id'],
-                            jsonObj['alternate_url'],   
-                            stri,                          
-                            jsonObj['schedule']['id'],
-                            jsonObj['area']['name']
-                        )
+                            vacancy_data = (
+                                user_id,
+                                user_query,
+                                v['url'],
+                                jsonObj['name'], 
+                                jsonObj['experience']['id'],
+                                jsonObj['alternate_url'],   
+                                stri,                          
+                                jsonObj['schedule']['id'],
+                                jsonObj['area']['name']
+                            )
 
-                        # print('Ready insert data: ', vacancy_data)
+                            # print('Ready insert data: ', vacancy_data)
 
-                        with conn.cursor() as cursor:
-                            cursor.execute(insert_query, vacancy_data)
+                            with conn.cursor() as cursor:
+                                cursor.execute(insert_query, vacancy_data)
+                                time.sleep(0.25)
+                                conn.commit()
+                                cursor.close()
+                                
+                            new_vacancy_count += 1
                             time.sleep(0.25)
-                            conn.commit()
-                            cursor.close()
-                            
-                        new_vacancy_count += 1
-                        time.sleep(0.25)
                     
                 else:
                     pass
@@ -182,6 +191,7 @@ def getVacnciesByType( l_countries, schedule = 'remote' ):
     print('\n\n Всего '+str(new_vacancy_count)+' новых вакансий по запросу ' + schedule)
 
 getVacnciesByType(countries,'fullDay')
+getVacnciesByType(countries,'remote')
 
 getVacnciesByType(schedule,'remote')
 
